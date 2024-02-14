d5dfb4dd9b03c083b3ee67b0d8dfaf69dd6244f0
Stoic By Us <159980841+StoicByUs@users.noreply.github.com>
2024-02-14 12:14:32 +0800

    Add files via upload

---
+++ assets/images/index.js
@@ -0,0 +1,90 @@
+import fields from "src/assets/images/cristina-anne-costello--vljYOhZnC0-unsplash.jpg";
+import stones from "deniz-altindas-t1XLQvDqt_4-unsplash.jpg";
+import gerald from "gerald.png";
+import ian from "ian.png";
+import merylle from "merylle.jpeg";
+import abstract from "milad-fakurian-GJKx5lhwU3M-unsplash.jpg";
+import nights from "undraw_night_calls_5jh7.png";
+import adopt from "./DisorderIllustrations/Adopt a pet-pana.png";
+import annoyed from "./DisorderIllustrations/Annoyed-rafiki.png";
+import beach from "./DisorderIllustrations/Beach-cuate.png";
+import breathe1 from "./DisorderIllustrations/Breathing exercise-bro.png";
+import breathe2 from "./DisorderIllustrations/Breathing exercise-cuate cyan.png";
+import breathe3 from "./DisorderIllustrations/Breathing exercise-cuate.png";
+import breathe4 from "./DisorderIllustrations/Breathing exercise-pana.png";
+import business from "./DisorderIllustrations/Business risk-bro.png";
+import couple from "./DisorderIllustrations/Couple stress-rafiki.png";
+import depressed from "./DisorderIllustrations/Depression-rafiki.png";
+import disco from "./DisorderIllustrations/Disco ball-rafiki.png";
+import escape from "./DisorderIllustrations/Escape-amico.png";
+import angry from "./DisorderIllustrations/Feeling angry-cuate.png";
+import feelblue from "./DisorderIllustrations/Feeling Blue-pana.png";
+import discussion from "./DisorderIllustrations/Group discussion-cuate.png";
+import therapy from "./DisorderIllustrations/Group therapy-rafiki.png";
+import internet from "./DisorderIllustrations/Internet on the go-cuate.png";
+import justice from "./DisorderIllustrations/Justice-amico.png";
+import meditate from "./DisorderIllustrations/Meditation-bro.png";
+import mindfulness from "./DisorderIllustrations/Mindfulness-bro.png";
+import moral from "./DisorderIllustrations/Moral-cuate.png";
+import music from "./DisorderIllustrations/Music-pana.png";
+import notes from "./DisorderIllustrations/Notes-bro.png";
+import ocd from "./DisorderIllustrations/Obsessive compulsive disorder-cuate.png";
+import ocpd from "./DisorderIllustrations/OCPD-bro.png";
+import personality from "./DisorderIllustrations/Personality disorder-amico.png";
+import positive from "./DisorderIllustrations/Positive thinking-cuate.png";
+import pinkptsd from "./DisorderIllustrations/Post-traumatic stress disorder-rafiki pink.png";
+import normalptsd from "./DisorderIllustrations/Post-traumatic stress disorder-rafiki.png";
+import broptsd from "./DisorderIllustrations/ptsd-bro.png";
+import running from "./DisorderIllustrations/Running-bro.png";
+import anxietybro from "./DisorderIllustrations/Social anxiety-bro.png";
+import anxietycuate from "./DisorderIllustrations/Social anxiety-cuate.png";
+import stress from "./DisorderIllustrations/Stress-pana.png";
+import stretching from "./DisorderIllustrations/Stretching exercises-amico.png";
+import sunrise from "./DisorderIllustrations/Sun rise-cuate.png";
+
+export {
+  fields,
+  stones,
+  gerald,
+  ian,
+  merylle,
+  abstract,
+  nights,
+  adopt,
+  annoyed,
+  beach,
+  breathe1,
+  breathe2,
+  breathe3,
+  breathe4,
+  business,
+  couple,
+  depressed,
+  disco,
+  escape,
+  angry,
+  feelblue,
+  discussion,
+  therapy,
+  internet,
+  justice,
+  meditate,
+  mindfulness,
+  moral,
+  music,
+  notes,
+  ocd,
+  ocd,
+  ocpd,
+  personality,
+  positive,
+  pinkptsd,
+  normalptsd,
+  broptsd,
+  running,
+  anxietybro,
+  anxietycuate,
+  stress,
+  stretching,
+  sunrise,
+};
